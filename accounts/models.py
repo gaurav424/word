1ff5230d407edeeecc05215a4e50aaf4377f6b7a
Gaurav <gaurav.rautela424@gmail.com>
2021-04-17 18:30:46 +0530

    First upload of project files

---
+++ accounts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	username = models.CharField(max_length = 200, null = True)
+	name = models.CharField(max_length = 200, null = True, blank=True)
+	phone = models.CharField(max_length = 200, null = True, blank=True)
+	email = models.CharField(max_length = 200, null = True, blank=True)
+	profile_pic = models.ImageField(default ="logo.png", null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return self.name
+
+
+class Survey(models.Model):
+	name = models.CharField(max_length = 200, null = True)
+	
+	def __str__(self):
+		return self.name
+
+	customer = models.ForeignKey(Customer, null = True, on_delete = models.SET_NULL)
+
+
+class Record(models.Model):
+	name = models.CharField(max_length = 200, null = True)
+	email = models.CharField(max_length = 200, null = True)
+	osat = models.CharField(max_length = 200, null = True)
+	comment = models.CharField(max_length = 1000, null = True)
+
+	def __str__(self):
+		return self.name
+
+	customer = models.ForeignKey(Customer, null = True, on_delete = models.SET_NULL)
---
+++ accounts/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .models import *
+from .forms import *
+
+# Create your views here.
+def registerPage(request):
+	form = CreateUserForm()
+	form2 = CustomerForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			
+			form_username = form.cleaned_data.get('username')
+			customer = Customer.objects.filter(username = form_username)
+			if not customer:
+				messages.info(request, form_username + ' is not authorized')
+			else:
+				user = form.save()
+				username = form.cleaned_data.get('username')
+			
+				messages.success(request, 'Account was created for ' + username)
+				return redirect('login')
+
+	context = {'form':form}
+	return render(request, 'word/register.html', context)
+
+
+def loginPage(request):
+	if request.method == "POST":
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.info(request, 'Username or password is incorrect')
+
+	context = {}
+	return render(request, 'word/login.html', context)
+
+
+def home(request):
+	context={}
+	return render(request,'word/dashboard.html', context)
